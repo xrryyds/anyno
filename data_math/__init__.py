1b2e0129fd4943aa3b3311fb58114bb9edfa1bae
xrryyds <xiongrengrong@kuaishou.com>
2026-04-15 06:24:14 +0800

    2

--- data_math/__init__.py
+++ data_math/__init__.py
@@ -3,10 +3,10 @@ from .MATH_500_data_util import Math_500
 from .GSM8K_data_util import GSM8K
 from .math_dataset import Math_DataSet
 from .math_data_util import Math_data
-from .AIME_2024_data_util import AIME2024
+from .AIME_data_util import AIME
 from .MATH_util import Math_All
 from .MATH_Sub_data_util import Math_Subset
 from .LiveMath_data_util import LiveMathBench
 
-__all__ = ["LoadDataset", "Math_500","GSM8K", "Math_DataSet", "Math_data", "AIME2024", "Math_All", "Math_Subset", "LiveMathBench"]
+__all__ = ["LoadDataset", "Math_500","GSM8K", "Math_DataSet", "Math_data", "AIME", "Math_All", "Math_Subset", "LiveMathBench"]
 
