9e9e1947ceafeeae1e7318c14099c4ebcbfba4e9
xrryyds <xiongrengrong@kuaishou.com>
2026-04-22 08:28:59 +0800

    2

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,6 +3,7 @@ from .inference.take_exam import TakeExam
 from .train.student_train_v2 import run_sira_training_v2
 from .train.student_train_v3 import run_sira_training_v3
 from .train.sft_train_baseline import run_sft_training_baseline
+from .train.sdft_baseline import run_sdft_training_baseline
 
 __all__= [
     "TeacherCorrecter",
@@ -10,5 +11,6 @@ __all__= [
     "run_sira_training_v2",
     "run_sft_baseline_training",
     "run_sira_training_v3",
-    "run_sft_training_baseline"
+    "run_sft_training_baseline",
+    "run_sdft_training_baseline"
 ]
